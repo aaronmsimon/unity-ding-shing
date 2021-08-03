0ad127447f402dbca8840cd2100ffadca2b8eda0
aaronmsimon <aaronsimon@gmail.com>
2021-08-02 22:30:44 -0700

    Initial commit
    Adding custom editor for ShingController so it's easier to adjust behaviors as the game grows and more capabilities are added for Shing

---
+++ 100 Ways to Ding Shing/Assets/Editor/ShingEditor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShingController))]
+public class ShingEditor : Editor
+{
+    private SerializedProperty speed;
+    private SerializedProperty groundLayer;
+
+    private SerializedProperty includePointOfInterest;
+    private SerializedProperty pointOfInterest;
+    private SerializedProperty returnPoint;
+
+    private void OnEnable()
+    {
+        speed = serializedObject.FindProperty("speed");
+        groundLayer = serializedObject.FindProperty("groundLayer");
+
+        includePointOfInterest = serializedObject.FindProperty("includePointOfInterest");
+        pointOfInterest = serializedObject.FindProperty("pointOfInterest");
+        returnPoint = serializedObject.FindProperty("returnPoint");
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //serializedObject.Update();
+        EditorGUILayout.PropertyField(speed);
+        EditorGUILayout.PropertyField(groundLayer);
+
+        EditorGUILayout.PropertyField(includePointOfInterest);
+        serializedObject.ApplyModifiedProperties();
+        if (includePointOfInterest.boolValue)
+        {
+            EditorGUILayout.PropertyField(pointOfInterest);
+            EditorGUILayout.PropertyField(returnPoint);
+        }
+    }
+}
