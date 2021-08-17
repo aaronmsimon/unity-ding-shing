b4b1a8c3538cb2951ffd3a4ecc4aec10207c4a68
aaronmsimon <aaronsimon@gmail.com>
2021-08-16 23:00:59 -0700

    Initial commit
    Got the coroutine working to flash at random intervals clamped to a range and a specified number of times
    Originally was trying to do this by enabling/disabling the component but worked better with the actual game object

---
+++ 100 Ways to Ding Shing/Assets/Scripts/Camera.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private GameObject flash;
+    [SerializeField] private int flashCount;
+
+    private int flashes;
+    private bool isFlashing;
+
+    private float minSeconds = .1f;
+    private float maxSeconds = .35f;
+    private float nextFlashSeconds;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            flashes = 0;
+            StartCoroutine(cameraFlash());
+        }
+    }
+
+    private IEnumerator cameraFlash()
+    {
+        while (flashes < flashCount)
+        {
+            isFlashing = !isFlashing;
+            flash.SetActive(isFlashing);
+            if (!isFlashing) flashes++;
+
+            nextFlashSeconds = Random.Range(minSeconds, maxSeconds);
+            yield return new WaitForSeconds(nextFlashSeconds);
+        }
+    }
+}
