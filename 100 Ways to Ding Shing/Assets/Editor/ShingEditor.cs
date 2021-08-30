7a59bc76b87446a8d699399a49fba72f23217a5d
aaronmsimon <aaronsimon@gmail.com>
2021-08-29 18:42:08 -0700

    Updated names and added Fall Threshold

--- 100 Ways to Ding Shing/Assets/Editor/ShingEditor.cs
+++ 100 Ways to Ding Shing/Assets/Editor/ShingEditor.cs
@@ -8,8 +8,9 @@ public class ShingEditor : Editor
 {
     private SerializedProperty speed;
     private SerializedProperty groundLayer;
+    private SerializedProperty fallDistThreshold;
 
-    private SerializedProperty includePointOfInterest;
+    private SerializedProperty includePOI;
     private SerializedProperty pointOfInterest;
     private SerializedProperty returnPoint;
 
@@ -18,8 +19,9 @@ public class ShingEditor : Editor
     {
         speed = serializedObject.FindProperty("speed");
         groundLayer = serializedObject.FindProperty("groundLayer");
+        fallDistThreshold = serializedObject.FindProperty("fallDistThreshold");
 
-        includePointOfInterest = serializedObject.FindProperty("includePointOfInterest");
+        includePOI = serializedObject.FindProperty("includePOI");
         pointOfInterest = serializedObject.FindProperty("pointOfInterest");
         returnPoint = serializedObject.FindProperty("returnPoint");
 
@@ -30,9 +32,10 @@ public class ShingEditor : Editor
         //serializedObject.Update();
         EditorGUILayout.PropertyField(speed);
         EditorGUILayout.PropertyField(groundLayer);
+        EditorGUILayout.PropertyField(fallDistThreshold);
 
-        EditorGUILayout.PropertyField(includePointOfInterest);
-        if (includePointOfInterest.boolValue)
+        EditorGUILayout.PropertyField(includePOI);
+        if (includePOI.boolValue)
         {
             EditorGUILayout.PropertyField(pointOfInterest);
             EditorGUILayout.PropertyField(returnPoint);
