472319693f7e5606168377595475e286c5ec8274
aaronmsimon <aaronsimon@gmail.com>
2021-09-03 17:48:11 -0700

    Added Ding sound for when you ding Shing

--- 100 Ways to Ding Shing/Assets/Scripts/DeathDetector.cs
+++ 100 Ways to Ding Shing/Assets/Scripts/DeathDetector.cs
@@ -6,10 +6,18 @@ public class DeathDetector : MonoBehaviour
 {
     [SerializeField] private GameObject levelComplete;
 
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Shing"))
         {
+            audioSource.Play();
             levelComplete.SetActive(true);
         }
     }
